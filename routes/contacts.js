d2e266e4166979f08575a0e8996de07b9b00c168
unknown <nduross1@gmail.com>
2019-11-13 02:21:47 -0800

    shipping

--- routes/contacts.js
+++ routes/contacts.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { isloggedIn, contactVal } = require('../middleware');
 const { createContact, getContacts, updateContact, deleteContact } = require('../controllers/contacts');
 
+//  contact/
 router.post('/', isloggedIn, contactVal, createContact);
 router.get('/', isloggedIn, getContacts);
 router.put('/:id', isloggedIn, contactVal, updateContact);
